4e71aabd3522a5d6e88a4450bdae953b5b3d1636
Jackson Ferguson <jackson.ferguson0@gmail.com>
2026-01-14 12:35:07 -0800

    feat: release git-pulsar v0.1.0

---
+++ Formula/git-pulsar.rb
@@ -0,0 +1,31 @@
+class GitPulsar < Formula
+  include Language::Python::Virtualenv
+
+  desc "Automated paranoid git backup for students"
+  homepage "https://github.com/JacksonFergusonDev/git-pulsar"
+  url "https://github.com/JacksonFergusonDev/git-pulsar/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a37471e43f23fa58472b69cc93b2ecad9f255c62fcb0ca0662a0ff1ebaef624c"
+  license "MIT"
+
+  depends_on "python"
+
+  def install
+    # 1. Create a virtualenv
+    # 2. Pip install requirements (if any) and the package itself
+    # 3. Link the binaries to Homebrew's bin folder
+    virtualenv_install_with_resources
+  end
+
+  service do
+    run [opt_bin/"git-pulsar-daemon"]
+    run_type :interval
+    interval 900
+    log_path var/"log/git-pulsar.log"
+    error_log_path var/"log/git-pulsar.err"
+    environment_variables PATH: std_service_path_env
+  end
+
+  test do
+    system bin/"git-pulsar", "--help"
+  end
+end
