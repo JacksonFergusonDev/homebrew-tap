31543921e544ea1db844c53e06def97a11b8c706
Jackson Ferguson <jackson.ferguson0@gmail.com>
2026-01-18 16:20:51 -0800

    git-pulsar 0.8.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/git-pulsar.rb
+++ Formula/git-pulsar.rb
@@ -3,8 +3,8 @@ class GitPulsar < Formula
 
   desc "Automated paranoid git backup for students"
   homepage "https://github.com/JacksonFergusonDev/git-pulsar"
-  url "https://github.com/JacksonFergusonDev/git-pulsar/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "d08efdc674b261cad2626796edd0769e8e299affa91e090dea4db215b248b5d3"
+  url "https://github.com/JacksonFergusonDev/git-pulsar/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "97eaa4f4ede128b2b4389c24186579ad9f07b061f8b613f04b886d627e470649"
   license "MIT"
 
   depends_on "python"
